b9b49673bc9ce4528d33571b4f18de91128a19fd
harshuthakur9731 <harshuthakur9731@gmail.com>
2025-05-20 18:23:14 +0530

    added fields specification and termsnconditions in product

--- ecommstore/models.py
+++ ecommstore/models.py
@@ -10,8 +10,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to = 'static', blank=True, null=True)
     price = models.DecimalField(max_digits=12,decimal_places=2)
     stock = models.IntegerField()
-    specification = models.TextField()
-    termsnconditions = models.TextField()
+    specification = models.TextField(null=True)
+    termsnconditions = models.TextField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
